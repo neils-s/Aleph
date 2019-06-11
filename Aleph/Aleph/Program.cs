098fcb4180d431345aba0c2a331282bddb5efd09
Neils Schoenfelder <neilsschoenfelder@gmail.com>
2019-06-10 20:21:22 -0500

    Add an empty project as a placeholder

---
+++ Aleph/Aleph/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aleph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
